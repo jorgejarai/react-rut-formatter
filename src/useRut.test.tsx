cdb7632158c1c0f3ba330e6871e5ef883fe8cc2b
Jorge Jara <jorgejarainostroza@gmail.com>
2022-07-15 21:23:59 -0400

    Cambios de formato

--- src/useRut.test.tsx
+++ src/useRut.test.tsx
@@ -1,31 +1,30 @@
-import { renderHook, act } from '@testing-library/react-hooks';
-import { useRut } from './useRut';
+import { renderHook, act } from "@testing-library/react-hooks";
 
-describe('useRut', () => {
-  it('properly receives and formats a given RUT number', () => {
-    const { result } = renderHook(() => useRut());
+import { useRut } from "./useRut";
 
-    act(() => {
-      result.current.updateRut('444444444');
-    });
+it("properly receives and formats a given RUT number", () => {
+  const { result } = renderHook(() => useRut());
 
-    expect(result.current.rut.raw).toBe('44444444-4');
-    expect(result.current.rut.formatted).toBe('44.444.444-4');
+  act(() => {
+    result.current.updateRut("444444444");
   });
 
-  it('checks if a RUT number is valid or not', () => {
-    const { result } = renderHook(() => useRut());
+  expect(result.current.rut.raw).toBe("44444444-4");
+  expect(result.current.rut.formatted).toBe("44.444.444-4");
+});
 
-    act(() => {
-      result.current.updateRut('444444444');
-    });
+it("checks if a RUT number is valid or not", () => {
+  const { result } = renderHook(() => useRut());
 
-    expect(result.current.isValid).toBeTruthy();
-
-    act(() => {
-      result.current.updateRut('444444445');
-    });
-
-    expect(result.current.isValid).toBeFalsy();
+  act(() => {
+    result.current.updateRut("444444444");
   });
+
+  expect(result.current.isValid).toBeTruthy();
+
+  act(() => {
+    result.current.updateRut("444444445");
+  });
+
+  expect(result.current.isValid).toBeFalsy();
 });
--- src/useRut.tsx
+++ src/useRut.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 import { prettifyRut, removeSeparators, calculateDv } from "./rutUtils";
 
 /**
